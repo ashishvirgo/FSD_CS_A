848ec5f7f7e1559f3183d337f15ab1a1c5b619f2
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-04-16 15:05:20 +0530

    user app created a

--- userapp/Server/Routes/userRoutes.js
+++ userapp/Server/Routes/userRoutes.js
@@ -2,4 +2,6 @@ const express=require('express');
 const usercontroller=require('../controller/userController')
 const router=express.Router();
 router.get("/users",usercontroller.getAllUsers);
+router.get("/user/:email",usercontroller.getUser);
+router.post("/createuser",usercontroller.createUser);
 module.exports=router;
--- userapp/Server/controller/userController.js
+++ userapp/Server/controller/userController.js
@@ -8,5 +8,26 @@ const getAllUsers=async(req,res)=>{
        res.status(500).json({message: err.message})
      }
 }
+const getUser=async(req,res)=>{
+    try{
+        const email=req.params.email;
+       const user= await User.find({email});
+       res.status(200).json(user);
+    }
+    catch(err){
+      res.status(500).json({message: err.message})
+    }
+}
+const createUser=async(req,res)=>{
+    try{
+        const {name,email,password,role}=req.body;
+       const newUser=new User({name,email,password,role});
+       const user=await newUser.save();
+       res.status(200).json(user);
+    }
+    catch(err){
+      res.status(500).json({message: err.message})
+    }
+}
 
-module.exports={getAllUsers}
+module.exports={getAllUsers,getUser,createUser}
