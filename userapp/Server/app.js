60dbb9c074548c2c358a817c4c0c6aa39a571e15
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-04-18 11:00:54 +0530

    updated backend app

--- userapp/Server/app.js
+++ userapp/Server/app.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const dotenv=require('dotenv');
+const cors=require("cors");
 const userroutes=require("./Routes/userRoutes");
 const dbConnect=require('./config/db')
 dotenv.config();
 const app=express();
 const Port=process.env.PORT || 3000;
 dbConnect();
+app.use(cors());
 app.use(express.json());
 app.use("/",userroutes)
 app.listen(Port,()=>{
